d306bdbe2ed53fb2b2caaf59e1993d74cd26f1d1
Will Barrett <will@tallgreentree.com>
2014-09-04 09:18:57 -0400

    Finished ex2

---
+++ ex2/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  puts("Hello world. \nYou are my sunshine.");
+  puts("I love my wife");
+  puts("very much. \nI like computers\nbecause they are stupid.");
+  return 0;
+}
